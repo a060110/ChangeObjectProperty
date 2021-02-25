7fbc9b1221d79d8350f86db82709ebd2349f07ed
a060110 <a060110@gmail.com>
2021-02-25 18:35:46 +0800

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(28, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(370, 116);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "變更label的Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(412, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(370, 116);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "變更label的BorderStyle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(28, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(370, 116);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "變更label的ForeBackColor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(412, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(370, 116);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "changeFontProperty";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MainLabel.ForeColor = System.Drawing.Color.Black;
+            this.MainLabel.Location = new System.Drawing.Point(259, 46);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(260, 48);
+            this.MainLabel.TabIndex = 1;
+            this.MainLabel.Text = "我叫賴群凱";
+            this.MainLabel.Click += new System.EventHandler(this.labelName_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 546);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "個人基本資料";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label MainLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MainLabel.Text == "正在學C#")
+            {
+                MainLabel.Text = "我叫賴群凱";
+            }
+            else
+            {
+                MainLabel.Text = "正在學C#";
+            }
+        }
+
+       private void labelName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MainLabel.BorderStyle==BorderStyle.None)
+            {
+                MainLabel.BorderStyle = BorderStyle.FixedSingle;
+            }
+            else
+            {
+                MainLabel.BorderStyle = BorderStyle.None;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MainLabel.ForeColor==Color.Black)
+            {
+                MainLabel.ForeColor = Color.White;
+                MainLabel.BackColor = Color.Black;
+            }
+            else
+            {
+                MainLabel.ForeColor = Color.Black;
+                MainLabel.BackColor = Color.White;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*if (MainLabel.Font==new Font("PMingLiU", 18pt,)
+            {
+
+            }*/
+            MainLabel.Font = new Font("Algerian", 22, FontStyle.Italic);
+        }
+    }
+}
